dd19acd1b7e1e75f2895b2089270928fc8c92835
jKyuery <kyugenes17@gmail.com>
2022-05-16 23:01:17 -0500

    finished the rest of components

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,15 +5,85 @@ class Contact extends Component{
 
         if(this.props.data){
             var name = this.props.data.name;
-            var street = this.props.data.street;
-            var city = this.props.data.city;
-            var state = this.props.data.state;
-            var zip = this.props.data.zip;
-            var phone = this.props.data.phone;
+            var street = this.props.data.address.street;
+            var city = this.props.data.address.city;
+            var state = this.props.data.address.state;
+            var zip = this.props.data.address.zip;
+            var phone= this.props.data.phone;
             var email = this.props.data.email;
-            //var resumeDownload = this.props.data.resumedownload;
+            var message = this.props.data.contactmessage;
         }
-    }
 
-    return()
+        return(
+            <section id = "contact">
+
+                <div className='row section-head'>
+
+                    <div className='two columns header-col'>
+                        <h1><span>Get in touch.</span></h1>
+                    </div>
+
+                    <div className='ten columns'>
+                        <p className='lead'>{message}</p>
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className = "eight columns">
+                        <form action='' method='post' id='contactForm' name='contactForm'>
+                            <fieldset>
+                                <div>
+                                    <label htmlFor='contactName'>Name <span className='required'>*</span></label>
+                                    <input type='text' defaultValue = "" size = "35" id = "contactName" name = "contactName" onChange={this.handleChange}/>
+                                </div>
+
+                                <div>
+                                    <label htmlFor='contactEmail'>Email<span className='required'>*</span></label>
+                                    <input type='text' defaultValue = "" size = "35" id = "contactEmail" name = "contactEmail" onChange={this.handleChange}/>
+                                </div>
+
+                                <div>
+                                    <label htmlFor = 'contactSubject'>Subject</label>
+                                    <input type='text' defaultValue = "" size = "35" id = "contactSubject" name = "contactSubject" onChange={this.handleChange}/>
+                                </div>
+
+                                <div>
+                                    <label htmlFor='contactMessage'>Message <span className='required'>*</span></label>
+                                    <textarea cols="50" rows = "15" id = "contactMessage" name = "contactMessage"></textarea>
+                                </div>
+
+                                <div>
+                                    <button className = "submit">Submit</button>
+                                    <span id = "image-loader">
+                                        <img alt = "" src = "images/loader.gif"/>
+                                    </span>
+                                </div>
+
+                            </fieldset>
+                        </form>
+
+                        <div id = "message-warning">Error!</div>
+                        <div id = "message-success">
+                            <i className='fa fa-check'></i>Your message was sent successfully!<br/>
+                        </div>
+
+                        <aside className='four columns footer'>
+                            <div className='widget widget_contact'>
+                                <h4>Address and Phone</h4>
+                                <p className='address'>
+                                    {name}<br/>
+                                    {street}<br/>
+                                    {city}, {state} {zip}<br/>
+                                    <span>{phone}</span>
+                                </p>
+                            </div>
+                        </aside>
+                    </div>
+                </div>
+
+            </section>
+        );
+    }
 }
+
+export default Contact;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,7 +3,40 @@ import React, {Component} from "react";
 class Projects extends Component {
     render() {
         if(this.props.data){
-            
+            var projects = this.props.data.projects.map(function(projects){
+                var projectImage = 'images/portfolio/' + projects.image;
+                return <div key={projects.title} className = "columns project-item">
+                    <div className="item-wrap">
+                        <a href={projectImages.url} title={projects.title}>
+                            <img alt={projects.title} src={projectImage} />
+                            <div className="overlay">
+                                <div className="project-item-meta">
+                                    <h5>{projects.title}</h5>
+                                        <p>{projects.category}</p>
+                                </div>
+                            </div>
+                            <div className="link-icon"><i classname= "fa fa-link"></i></div>
+                        </a>
+                    </div>
+                </div>
+            })
         }
+
+        return(
+            <section id = "portfolio">
+                <div className="row">
+                    <div className="twelve columns collapsed">
+                        <h1>My Projects below.</h1>
+
+                        <div id = "projects-wrapper" className ="bgrid-quarters s-bgrid-thirds cf">
+                            {projects}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+
     }
 }
+
+export default Projects;
