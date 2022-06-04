db3cd26a54d86cb7b52f2b5121530a745a5190bd
jKyuery <kyugenes17@gmail.com>
2022-06-03 20:35:02 -0500

    implemented app.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component{
                     <ul className='copyright'>
                         <li>&copy; Copyright 2022 Kyu Shim</li>
                         <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-                        <li>Template provided by <a title='credit' href="https://github.com/tbakerx/react-resume-template">Tim Baker</a></li>
+                        <li>Template by <a title='credit' href="https://github.com/tbakerx/react-resume-template">Tim Baker</a></li>
                     </ul>
                 </div>
                 <div id="go-top"><a className='smoothscroll' title='Back to Top' href='"#home'><i className='icon-up-open'></i></a></div>
