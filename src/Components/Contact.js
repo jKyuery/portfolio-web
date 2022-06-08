fc8765435cbff69e43a9f3afbb65706ce84298e2
jKyuery <kyugenes17@gmail.com>
2022-06-07 20:08:15 -0500

    fixed contact section

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,7 +5,6 @@ class Contact extends Component{
 
         if(this.props.data){
             var name = this.props.data.name;
-            var street = this.props.data.address.street;
             var city = this.props.data.address.city;
             var state = this.props.data.address.state;
             var zip = this.props.data.address.zip;
@@ -16,9 +15,7 @@ class Contact extends Component{
 
         return(
             <section id = "contact">
-
                 <div className='row section-head'>
-
                     <div className='two columns header-col'>
                         <h1><span>Get in touch.</span></h1>
                     </div>
@@ -29,16 +26,16 @@ class Contact extends Component{
                 </div>
 
                 <div className='row'>
-                    <div className = "eight columns">
+                    <div className='eight columns'>
                         <form action='' method='post' id='contactForm' name='contactForm'>
                             <fieldset>
                                 <div>
-                                    <label htmlFor='contactName'>Name <span className='required'>*</span></label>
-                                    <input type='text' defaultValue = "" size = "35" id = "contactName" name = "contactName" onChange={this.handleChange}/>
+                                    <label htmlFor='contactName'>Name<span className='required'>*</span></label>
+                                    <input type = 'text' defaultValue = "" size = "35" id= "contactName" name = "contactName" onChange={this.handleChange}/>
                                 </div>
 
                                 <div>
-                                    <label htmlFor='contactEmail'>Email<span className='required'>*</span></label>
+                                    <label htmlFor="contactEmail">Email <span className='required'>*</span></label>
                                     <input type='text' defaultValue = "" size = "35" id = "contactEmail" name = "contactEmail" onChange={this.handleChange}/>
                                 </div>
 
@@ -53,12 +50,11 @@ class Contact extends Component{
                                 </div>
 
                                 <div>
-                                    <button className = "submit">Submit</button>
+                                    <button className='submit'>Submit</button>
                                     <span id = "image-loader">
                                         <img alt = "" src = "images/loader.gif"/>
                                     </span>
                                 </div>
-
                             </fieldset>
                         </form>
 
@@ -67,20 +63,20 @@ class Contact extends Component{
                             <i className='fa fa-check'></i>Your message was sent successfully!<br/>
                         </div>
 
-                        <aside className='four columns footer'>
-                            <div className='widget widget_contact'>
-                                <h4>Address and Phone</h4>
-                                <p className='address'>
-                                    {name}<br/>
-                                    {street}<br/>
-                                    {city}, {state} {zip}<br/>
-                                    <span>{phone}</span>
-                                </p>
-                            </div>
-                        </aside>
                     </div>
-                </div>
 
+                    <aside className='four columns footer-widgets'>
+                        <div className='widget widget_contact'>
+                            <h4>Address and Phone</h4>
+                            <p className='address'>
+                                {name}<br/>
+                                {city}<br/>
+                                {state},{zip}<br/>
+                                <span>{phone}</span><br/>
+                            </p>
+                        </div>
+                    </aside>
+                </div>
             </section>
         );
     }
