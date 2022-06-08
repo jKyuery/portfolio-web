72b113c7614ca0c35a5dd380a8d5b39047ea35db
jKyuery <kyugenes17@gmail.com>
2022-06-07 22:50:24 -0500

    neatified tech stack section

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -65,7 +65,7 @@ class Resume extends Component{
 
                 <div className='row skill'>
                     <div className = "three columns header-col">
-                        <h1><span>Skills</span></h1>
+                        <h1><span>Tech Stack</span></h1>
                     </div>
                 
                 <div>
