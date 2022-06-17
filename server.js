4d0a18088a870fca6be4a353e19b768644678da0
jKyuery <kyugenes17@gmail.com>
2022-06-16 20:52:14 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(express.static('./public'));
 
 // This route serves the React app
-app.get('*', (req, res) => res.sendFile(path.join(publicPath, "index.html")));
+app.get('*', (req, res) => res.sendFile(('./public/index.html'));
 
 app.listen(port, () => console.log(`Server up!`));
