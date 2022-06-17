25c61dd7350249c8ceb18c413b590f44f5c0cdc6
jKyuery <kyugenes17@gmail.com>
2022-06-16 21:15:53 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
+app.use('/static', express.static('./public'));
 
 // This route serves the React app
 app.get('*', (req, res) => res.sendFile(('./public/index.html')));
