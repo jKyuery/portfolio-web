73f1baa1f5372047f616f99fccb9f9eb41cf0005
jKyuery <kyugenes17@gmail.com>
2022-06-16 20:32:53 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 // eslint-disable-next-line no-unused-vars
 // const bodyParser = require('body-parser');
-const publicPath = path.join(__dirname, '..', 'public')
 const path = require("path");
 const app = express();
+const publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
@@ -11,4 +11,4 @@ app.use(express.static(publicPath));
 // This route serves the React app
 app.get('*', (req, res) => res.sendFile(path.resolve(publicPath, "index.html")));
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(port, () => console.log(`Server up!`));
